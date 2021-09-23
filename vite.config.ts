499518f2fe70240046922a840f089a02f2dfe936
Fernando Daciuk <f.daciuk@gmail.com>
2021-09-23 10:40:24 -0300

    Add vite config for TS custom paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import tsConfigpaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(), tsConfigpaths()],
+  esbuild: {
+    jsxInject: `import * as React from 'react'`
+  }
 })
